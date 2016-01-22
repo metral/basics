de80820f0138edc3755d640c6b3ed8c92e418480
Mike Metral <metral@gmail.com>
2016-01-22 12:24:46 -0800

    split init into gb & wercker scripts

---
+++ init_local_wercker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# create dirs for local wercker build & deploy logs
+mkdir -p _output/builds
+mkdir -p _output/deploys
---
+++ init_manual_gb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# create dir for manual gb vendoring of a library
+mkdir -p vendor/src
