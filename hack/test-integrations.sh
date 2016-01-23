910d9c174fe3165bcba9e665162107b4a3b12fae
Mike Metral <metral@gmail.com>
2016-01-22 16:11:16 -0800

    make generic for any golang gb proj

--- hack/test-integrations.sh
+++ hack/test-integrations.sh
@@ -8,9 +8,30 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# TODO set CONF_FILE
-# TODO set GO_PKG
+SCRIPTS_ROOT=$(dirname "${BASH_SOURCE}")
 
+GB_PROJ_ROOT=`readlink -f $1`
+GB_PROJ_SRC=$GB_PROJ_ROOT/src
+
+# find all *_test.go files in GB_PROJ_SRC that ARE in the -path / -o path's
+# specified
+test::find_dirs() {
+(
+    cd ${GB_PROJ_SRC}
+    find . \( \
+        \( \
+        -path '*/tests/integration/*' \
+        \) -prune \
+        \) -name '*_test.go' -print0 | xargs -r -0n1 dirname | sed 's|^\./||' | sort -u
+    )
+}
+
+
+testcases=()
+testcases=($(test::find_dirs))
+
+# iterate through all of the golang integration tests in testcases
+for pkg in "${testcases[@]:+${testcases[@]}}"; do
 TEST_INTEGRATION="true" \
-    ./test-go.sh \
-    $GO_PKG/tests/integration
+    $SCRIPTS_ROOT/test-go.sh $GB_PROJ_ROOT ${pkg}
+done
